7af1cd9665eca58c6dbcdced39d068bda2018e1d
thainq3127 <thainq3127@gmail.com>
2021-08-17 10:48:13 +0700

    Updates

---
+++ _next/static/vTPjSITXbdE3vYPWcHVJ9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-e5dbc78e6dcf82065cff.js"],"/_error":["static/chunks/pages/_error-6dc97a68f6d8f2060458.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
